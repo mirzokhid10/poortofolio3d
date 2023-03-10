d84ddfc081da2791dd187c815ef75c68127f7a91
Mirzokhid10 <mxudoyberdiyev21@gmail.com>
2023-03-10 02:02:37 +0100

    finesh

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,13 +2,15 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import ConvasLoader from "../Loader";
+import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
-      <hemisphereLight
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
@@ -56,13 +58,10 @@ const ComputersCanvas = () => {
       frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{
-        position: [20, 3, 5],
-        fov: 25,
-      }}
+      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<ConvasLoader />}>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
@@ -70,9 +69,10 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
 };
 
-export default Computers;
+export default ComputersCanvas;
