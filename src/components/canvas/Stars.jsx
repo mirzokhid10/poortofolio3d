7a7fcdf806c7cd494a1087c67ff1355d72773469
Mirzokhid10 <mxudoyberdiyev21@gmail.com>
2023-03-10 02:07:05 +0100

    fixed star page

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,6 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
